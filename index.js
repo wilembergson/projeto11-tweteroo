8912c4ec3bad79ca6e4bce21e04938e9d6684aef
wilembergson <wilembergson@yahoo.com>
2022-04-22 16:32:34 -0300

    Sign-up

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+let user = {}
+const users = []
 
 app.post("/sign-up", (req, res)=>{
-    res.send("testando post")
+    user = { username: req.body.username, avatar: req.body.avatar }
+    users.push(user)
+    res.send("OK")
 })
 
-app.listen(5000)
+app.listen(5000, ()=>{
+    console.log("Running...")
+})
